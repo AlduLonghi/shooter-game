b16df91dc7e99873f7d2860962fbda70a8278a31
Aldana Abril Longhi <aldulonghi@gmail.com>
2020-12-18 12:16:08 -0300

    Add scores to scoresscene

--- src/helpers/send-score.js
+++ src/helpers/send-score.js
@@ -7,12 +7,12 @@ const sendScore = (name, score, scene) => {
 
     fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/nFeS9gyeO4tJflNcgAJU/scores/', {
         method: 'POST',
-        mode: 'cors',
         body: JSON.stringify(sendScore),
         headers:{
         'Content-Type': 'application/json'
         }
-    }).then(() => scene.scene.start('PlayerName'))
+    }).then(res => {console.log(res);
+        scene.scene.start('PlayerName')})
     .catch(error => console.error('Error:', error));
 }
 
--- src/scenes/scoresScene.js
+++ src/scenes/scoresScene.js
@@ -9,7 +9,13 @@ class ScoresScene extends Phaser.Scene {
     }
 
     create() {
-        this.add.text(230, 230, this.scores[0].user, { fontSize: '75px', fill: '#ffffff' });
+        this.text = this.add.text(230, 150, 'Top scores', { fontSize: '65px', fill: '#ffffff' });
+        
+        let height = 240;
+
+        for (let i = 0; i<this.scores.length; i += 1) {
+          this.add.text(260, height += 60, `${i+1}. ${this.scores[i].user}: ${this.scores[i].score}`, { fontSize: '40px', fill: '#ffffff' })
+        }
     }
 }
 
